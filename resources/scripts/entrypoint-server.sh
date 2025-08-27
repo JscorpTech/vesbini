9a61207d62c266a8d736bd794a2dca1e88eb1865
A'zamov Samandar <admin@jscorp.uz>
2025-08-27 14:02:42 +0500

    fix typo

--- resources/scripts/entrypoint-server.sh
+++ resources/scripts/entrypoint-server.sh
@@ -2,7 +2,7 @@
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 
-gunicorn config.wsgi:application -b 0.0.0.0:8000 --workers $(($(nproc) * 2 + 1))
+gunicorn config.wsgi:application -b 0.0.0.0:8000 --workers 4 # $(($(nproc) * 2 + 1)) 
 
 
 
