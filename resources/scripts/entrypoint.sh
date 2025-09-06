1c10bc9e4f3a4139ce92f22e35a497af2b27c19f
A'zamov Samandar <admin@jscorp.uz>
2025-09-06 15:35:54 +0500

    entrypoint updated

--- resources/scripts/entrypoint.sh
+++ resources/scripts/entrypoint.sh
@@ -7,6 +7,7 @@ done
 
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
+python3 manage.py compilemessages
 
 uvicorn config.asgi:application --host 0.0.0.0 --port 8000 --reload --reload-dir core --reload-dir config
 
