c21707d6f54785086d38e0a4c18f6d8580718074
A'zamov Samandar <admin@jscorp.uz>
2025-09-10 18:51:59 +0500

    fix typo

--- config/conf/payme.py
+++ config/conf/payme.py
@@ -4,6 +4,7 @@ PAYME_ID = env.str("PAYME_ID")
 PAYME_KEY = env.str("PAYME_KEY")
 PAYME_ACCOUNT_FIELD = "id"
 PAYME_ACCOUNT_MODEL = "core.apps.api.models.order.OrderModel"
-PAYME_ONE_TIME_PAYMENT = False
+PAYME_AMOUNT_FIELD = "amount"
+PAYME_ONE_TIME_PAYMENT = True
 
 PAYME_DISABLE_ADMIN = False
