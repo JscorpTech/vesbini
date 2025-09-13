8c0919f847e33b383eb947e23ccf655bf72ee2d2
A'zamov Samandar <admin@jscorp.uz>
2025-09-13 17:51:25 +0500

    feat: cache enabled

--- config/conf/cache.py
+++ config/conf/cache.py
@@ -17,10 +17,10 @@ CACHEOPS_DEFAULTS = {
 }
 CACHEOPS = {
     # !NOTE: api => "you app name"
-    # "api.*": {
-    #     "ops": "all",  # Barcha turdagi so'rovlarni keshga olish
-    #     "timeout": 60 * 5,  # 5 daqiqa davomida saqlash
-    # },
+    "api.*": {
+        "ops": "all",  # Barcha turdagi so'rovlarni keshga olish
+        "timeout": 60 * 5,  # 5 daqiqa davomida saqlash
+    },
 }
 CACHEOPS_DEGRADE_ON_FAILURE = True
 CACHEOPS_ENABLED = env.bool("CACHE_ENABLED", False)
