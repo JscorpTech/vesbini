136102ec425b71edff647cf48c646e063d53f3e5
A'zamov Samandar <admin@jscorp.uz>
2025-09-13 17:23:29 +0500

    entrypoint updated

--- resources/scripts/entrypoint-server.sh
+++ resources/scripts/entrypoint-server.sh
@@ -9,7 +9,7 @@ python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 python3 manage.py compilemessages
 
-gunicorn config.wsgi:application -b 0.0.0.0:8000 --workers 4 # $(($(nproc) * 2 + 1)) 
+gunicorn config.wsgi:application -b 0.0.0.0:8000 --workers $(($(nproc) * 2 + 1)) 
 
 
 
