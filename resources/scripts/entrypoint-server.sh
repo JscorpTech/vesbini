557baecea465ea68e8905648883191691bb68d72
A'zamov Samandar <admin@jscorp.uz>
2025-09-15 21:37:30 +0500

    fix: worker count

--- resources/scripts/entrypoint-server.sh
+++ resources/scripts/entrypoint-server.sh
@@ -9,7 +9,7 @@ python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 python3 manage.py compilemessages
 
-gunicorn config.wsgi:application -b 0.0.0.0:8000 --workers 16 --worker-class gevent --worker-connections 10000 # $(($(nproc) * 2 + 1)) 
+gunicorn config.wsgi:application -b 0.0.0.0:8000 --workers 6 --worker-class gevent --worker-connections 10000 # $(($(nproc) * 2 + 1)) 
 
 exit $?
 
