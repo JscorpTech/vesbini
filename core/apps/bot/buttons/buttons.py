fb18540e9d8b0c4103baa460c5c6c12efd321899
A'zamov Samandar <admin@jscorp.uz>
2025-09-16 16:54:09 +0500

    fix typo

--- core/apps/bot/buttons/buttons.py
+++ core/apps/bot/buttons/buttons.py
@@ -1,9 +1,9 @@
-from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, WebAppInfo
 
 from core.apps.bot.services import get_message as _
 
 
 def home():
     button = InlineKeyboardMarkup()
-    button.add(InlineKeyboardButton(_("mini_app"), web_app={"url": "https://vesbini-latest.vercel.app/"}))
+    button.add(InlineKeyboardButton(_("mini_app"), web_app=WebAppInfo(url="https://vesbini-latest.vercel.app/")))
     return button
