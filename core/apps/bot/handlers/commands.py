7e8c61aa08f197a66fd15f73036c8be94b7fca86
A'zamov Samandar <admin@jscorp.uz>
2025-09-16 16:28:53 +0500

    updated

--- core/apps/bot/handlers/commands.py
+++ core/apps/bot/handlers/commands.py
@@ -6,4 +6,4 @@ from core.apps.bot.services import get_message as _
 
 @bot.message_handler(commands=["start"])
 def start(msg: Message):
-    bot.send_message(msg.chat.id, _("Assalomu Alaykum botga hush kelibsiz!!!"))
+    bot.send_message(msg.chat.id, _("start_message"))
