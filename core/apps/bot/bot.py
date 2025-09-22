cccf9080ec654fd2c5a831777e0efc2451f5c1af
A'zamov Samandar <admin@jscorp.uz>
2025-09-22 14:46:18 +0500

    fix typo

--- core/apps/bot/bot.py
+++ core/apps/bot/bot.py
@@ -5,6 +5,7 @@ from telebot.storage.redis_storage import StateRedisStorage
 
 from config.env import env
 from core.apps.bot.filters import MessageFilter, PageFilter
+from core.apps.bot.filters.message import IsAdminFilter
 from core.apps.bot.middlewares import LocaleMiddleware
 
 try:
@@ -18,6 +19,7 @@ try:
     bot.add_custom_filter(custom_filters.StateFilter(bot))
     bot.add_custom_filter(MessageFilter())
     bot.add_custom_filter(PageFilter())
+    bot.add_custom_filter(IsAdminFilter())
     bot.setup_middleware(LocaleMiddleware())
 except Exception as e:
     logging.error("Bot variable yaratilmadi")
