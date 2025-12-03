cd51ff52eacc025874a5482e0412cf372026f853
A'zamov Samandar <admin@jscorp.uz>
2025-12-03 18:29:56 +0500

    fix typo

--- resources/scripts/celery-beat.sh
+++ resources/scripts/celery-beat.sh
@@ -6,10 +6,6 @@ while ! nc -z $DB_HOST $DB_PORT; do
   sleep 2
   echo "Waiting postgress...."
 done
-while ! nc -z $DB_HOST $DB_PORT; do
-  sleep 2
-  echo "Waiting redis...."
-done
 
 python3 manage.py compilemessages
 celery -A config beat -l info
